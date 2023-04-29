daa4d0109ca780d8208176ec186d38952ddcc013
Nataliia Hladii <natalia14101995@gmail.com>
2023-04-29 12:05:12 +0200

    vytváření komponenty Drink

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,6 @@
 import './style.css' ;
+import { Drink } from './components/Drink/Drink.js';
+
 
 export const Menu = () => {
 
@@ -12,69 +14,6 @@ export const Menu = () => {
                 Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
             </p>
             <div class="drinks-list">
-                <div class="drink">
-                <div class="drink__product">
-                    <div class="drink__cup">
-                    <img src="https://cafelora.kodim.app/assets/cups/espresso.png">
-                    </div>
-                    <div class="drink__info">
-                    <h3>Espresso</h3>
-                    <div class="layer">
-                        <div class="layer__color" style="background-color: #613916"></div>
-                        <div class="layer__label">espresso</div>
-                    </div>
-                    </div>
-                </div>
-                <div class="drink__controls">
-                    <button class="order-btn">
-                    Objednat
-                    </button>
-                </div>
-                </div>
-
-                <div class="drink">
-                <div class="drink__product">
-                    <div class="drink__cup">
-                    <img src="https://cafelora.kodim.app/assets/cups/doppio.png">
-                    </div>
-                    <div class="drink__info">
-                    <h3>Doppio</h3>
-                    <div class="layer">
-                        <div class="layer__color" style="background-color: #613916"></div>
-                        <div class="layer__label">espresso</div>
-                    </div>
-                    </div>
-                </div>
-                <div class="drink__controls">
-                    <button class="order-btn">
-                    Objednat
-                    </button>
-                </div>
-                </div>
-
-                <div class="drink">
-                <div class="drink__product">
-                    <div class="drink__cup">
-                    <img src="https://cafelora.kodim.app/assets/cups/lungo.png">
-                    </div>
-                    <div class="drink__info">
-                    <h3>Lungo</h3>
-                    <div class="layer">
-                        <div class="layer__color" style="background-color: #b0dee1"></div>
-                        <div class="layer__label">voda</div>
-                    </div>
-                    <div class="layer">
-                        <div class="layer__color" style="background-color: #613916"></div>
-                        <div class="layer__label">espresso</div>
-                    </div>
-                    </div>
-                </div>
-                <div class="drink__controls">
-                    <button class="order-btn">
-                    Objednat
-                    </button>
-                </div>
-                </div>
             </div>
 
             <div class="order-detail">
@@ -83,5 +22,17 @@ export const Menu = () => {
         </div>
     `
 
+    element.querySelector('.drinks-list').append(
+        Drink({id: '1', name: 'romano', ordered: true, image: 'https://cafelora.kodim.app/assets/cups/romano.png', layers: [
+            {
+              color: '#fbdf5b',
+              label: 'citrón',
+            },
+            {
+              color: '#613916',
+              label: 'espresso',
+            },
+          ]})
+    )
     return element
 }
