7a71cda9501c5a5469fc8ec67f82759729e0eda0
Nataliia Hladii <natalia14101995@gmail.com>
2023-04-29 11:30:54 +0200

    Vytváření komponent, zprovoznění navigace

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+import { Header } from './components/Header/Header.js';
+import { Banner } from './components/Banner/Banner.js';
+import { Menu } from './components/Menu/Menu.js';
+import { Gallery } from './components/Gallery/Gallery.js';
+import { Contact } from './components/Contact/Contact.js';
+import { Footer } from './components/Footer/Footer.js';
+
+const pageElement = document.createElement('div');
+pageElement.classList.add('page');
+
+const main = document.createElement('main');
+main.append(Banner(), Menu(), Gallery(), Contact());
+
+pageElement.append(Header(), main, Footer());
+
+document.querySelector('#app').append(pageElement);
